b2b67580fd6e0b7d42b24fb15a1da3263f3e7c47
Felix <121625899+stellariumImpl@users.noreply.github.com>
2024-07-12 17:31:46 +1000

    Update transforms.py

--- utils/transforms.py
+++ utils/transforms.py
@@ -3,6 +3,7 @@
 import numpy as np
 import random
 import torch
+from torchvision import transforms
 import torchvision.transforms.functional as TF
 from PIL import Image
 
@@ -79,6 +80,49 @@ class RandomCutout:
             image *= mask
         return image
 
+class PairNormalizeToTensor(object):
+    def __init__(self, norm=True, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+        """
+        IMAGE_NORM_MEAN = [0.485, 0.456, 0.406]  # ImageNet统计的RGB mean
+        IMAGE_NORM_STD = [0.229, 0.224, 0.225]  # ImageNet统计的RGB std
+        LABEL_NORM_MEAN = [0.5]  # ImageNet统计的GRAY mean
+        LABEL_NORM_STD = [0.5]  # ImageNet统计的GRAY std
+        :param norm: 是否正则化，默认是
+        :param mean: 正则化的平均值mean
+        :param std: 正则化的标准差std
+        """
+        super(PairNormalizeToTensor, self).__init__()
+        self.norm = norm
+        self.mean = mean
+        self.std = std
+        pass
+
+    def __call__(self, image, label=None):
+        """
+        归一化，只对image除以255，label不动
+        :param image: [H,W,C] PIL Image RGB 0~255
+        :param label: [H,W] PIL Image trainId
+        :return: [C,H,W] tensor RGB -1.0~0.0,  [H,W] tensor trainId
+        """
+        # torchvision.transform的API，对PIL Image类型image归一化，也就是除以255
+        # 并转为tensor，维度变为[C,H,W]
+        # image [C,H,W]tensor RGB 0.0~1.0
+        image = TF.to_tensor(image)
+
+        # 正则化，x=(x-mean)/std
+        # 只对image正则化, image [C,H,W]tensor RGB -1.0~1.0
+        if self.norm:
+            image = TF.normalize(image, self.mean, self.std)
+
+        # 先转为ndarray，再转为tensor，不归一化，维度保持不变
+        # label [H,W]tensor trainId
+        if label is not None:
+            label = torch.from_numpy(np.asarray(label))
+
+        return image, label
+
+    pass
+
 class TrainTransform:
     def __init__(self):
         self.crop = PairCrop(offsets=(690, None))
@@ -97,6 +141,23 @@ class TrainTransform:
         image = self.cutout(image)
         return image, label
 
+class TrainTransform:
+    def __init__(self, size=256):
+        self.pair_resize = PairResize(size)
+        self.image_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+    def __call__(self, image, label):
+        image, label = self.pair_resize(image, label)
+        image = self.image_transform(image)
+        label = torch.from_numpy(np.array(label)).long()
+        return image, label
+
+# class TestTransform(TrainTransform):
+#     # For this example, we're using the same transform for test as for train
+#     pass
 class TestTransform:
     def __init__(self):
         self.crop = PairCrop(offsets=(690, None))
