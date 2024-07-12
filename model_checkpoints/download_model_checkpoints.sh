ef2ef0f285f6ade0e4a1555163fabea7b6459b07
Felix <121625899+stellariumImpl@users.noreply.github.com>
2024-07-12 18:16:42 +1000

    Add files via upload

---
+++ model_checkpoints/download_model_checkpoints.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://www.dropbox.com/scl/fi/d27ve7x8bvpej9f7jzek0/best_model_epoch_28.pth?rlkey=7j7epj57kibzhhrgb129x3elv&st=7hwp7wr6&dl=1 -O best_model_epoch_28.pth
+wget https://www.dropbox.com/scl/fi/y1sub7c1awkgrxnt8k7cl/best_model_epoch_47.pth?rlkey=jv8jo40w679k0fznvww77nc5t&st=3wxnmb5r&dl=1 -O best_model_epoch_47.pth
