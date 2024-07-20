ef52829d214cabb0d3869b36f0221413b407fb42
Felix <121625899+stellariumImpl@users.noreply.github.com>
2024-07-20 17:42:58 +1000

    Delete model_checkpoints directory

--- model_checkpoints/download_model_checkpoints.sh
+++
@@ -1,5 +0,0 @@
-curl -L "https://www.dropbox.com/scl/fi/wr0nug4wgaeg9qtt7hrol/best_model_epoch_26.pth?rlkey=2fq9mowc5xi5a047clv10mfcv&st=zq2gdsxq&dl=1" -o best_model_epoch_26.pth
-
-curl -L "https://www.dropbox.com/scl/fi/d27ve7x8bvpej9f7jzek0/best_model_epoch_28.pth?rlkey=7j7epj57kibzhhrgb129x3elv&st=7hwp7wr6&dl=1" -o best_model_epoch_28.pth
-
-curl -L "https://www.dropbox.com/scl/fi/y1sub7c1awkgrxnt8k7cl/best_model_epoch_47.pth?rlkey=jv8jo40w679k0fznvww77nc5t&st=3wxnmb5r&dl=1" -o best_model_epoch_47.pth
