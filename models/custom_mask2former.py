35011f4179625fe12982ea32b2591e74e7c56950
Felix <121625899+stellariumImpl@users.noreply.github.com>
2024-07-31 20:22:10 +1000

    Update custom_mask2former.py

--- models/custom_mask2former.py
+++ models/custom_mask2former.py
@@ -8,23 +8,23 @@ class CustomMask2Former(nn.Module):
         self.mask2former = Mask2FormerForUniversalSegmentation.from_pretrained(
             "facebook/mask2former-swin-base-coco-panoptic")
         
-        # 凍結大部分參數
+        # freezing most part of parameters
         for param in self.mask2former.parameters():
             param.requires_grad = False
         
-        # 解凍最後幾層
+        # unfrozen the last conv layer
         trainable_layers = list(self.mask2former.named_children())[-5:]
         for name, layer in trainable_layers:
             for param in layer.parameters():
                 param.requires_grad = True
 
-        # 修改輸出層以適應類別數
+        # modify the output layer to accommodate the number of classes
         self.mask2former.config.num_labels = num_classes
         if hasattr(self.mask2former, 'class_predictor'):
             in_features = self.mask2former.class_predictor.in_features
             self.mask2former.class_predictor = nn.Linear(in_features, num_classes)
 
-        # 添加額外的卷積層
+        # adding additional convolution layers
         self.extra_conv = nn.Conv2d(100, num_classes, kernel_size=3, padding=1)
 
     def forward(self, pixel_values):
